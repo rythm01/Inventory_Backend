85e4fc8273dc59f91f72e6e6c80c3b6a3579a467
rythm01 <ridhamchauhan693@gmail.com>
2025-01-08 08:00:13 +0530

    #Added unit of work

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//builder.Services.AddScoped<IUnitOfWork, UnitofWork>();
+builder.Services.AddScoped<IUnitOfWork, UnitofWork>();
 // Add services to the container.
 
 builder.Services.AddControllers();
